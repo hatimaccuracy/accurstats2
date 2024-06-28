1a928420b63fe81c098701a8ff22b6992bb95b6c
hatimaccuracy <hatim.blilet@accuracy.com>
2024-06-28 14:50:41 +0200

    Update config.py

--- accurstats_v2/config.py
+++ accurstats_v2/config.py
@@ -1,3 +1,2 @@
-OPEN_AI_KEY = "API"
-yapping_interpret = ". Ce que je te demande de faire c'est de proposer une interpretation du scénarios economique que ce modèle suggère , c'est à dire justifier les variables proposer et les signes de leurs coefficients d'une façon à décrire les scénarios économiques engendré par les changement des variables"
-retrieval_methods = ['', 'Yahoo Finance', 'Banque de France', 'Banque Européenne Centrale', 'Importer un fichier tabulaire localement']
+OPEN_AI_KEY = "NO_API_KEY_FOR_YOU"
+BDF_API_KEY = "NO_API_KEY_FOR_YOU"
